b4f566422b4fe1cfdc6ed10b78de4ed1a69a4434
mr-crockpot <adamschor@adams-macbook-air.local>
2018-11-22 11:13:57 -0700

    Initial Commit

---
+++ Tiles/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tiles
+//
+//  Created by Adam Schor on 11/22/18.
+//  Copyright © 2018 AandA Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
